7bbc4f91b5460d7db4bb15cbea3a193be24ea540
ngetahun <n.getahun@jacobs-university.de>
2017-04-16 03:02:11 +0200

    Refactored main function

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
 
 int main(int argc, char *argv[])
 {
-    fprintf(stdout, argv[0], "%s Version %d.%d\n", ukuleleFactory_VERSION_MAJOR, ukuleleFactory_VERSION_MINOR);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
